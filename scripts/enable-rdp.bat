324babf6a087dd369bb9b440a09d94b79aba5b78
Ryan Hansohn <ryan.hansohn@gmail.com>
2015-03-31 15:29:27 -0700

    add debug handling

--- scripts/enable-rdp.bat
+++ scripts/enable-rdp.bat
@@ -1,2 +1,15 @@
+@setlocal EnableDelayedExpansion EnableExtensions
+@for %%i in (a:\_packer_config*.cmd) do @call "%%~i"
+@if defined PACKER_DEBUG (@echo on) else (@echo off)
+
+title Enabling Remote Desktop. Please wait...
+
+:: Open firewall port 3389
+echo ==^> Openning Firewall Port 3389
 netsh advfirewall firewall add rule name="Open Port 3389" dir=in action=allow protocol=TCP localport=3389
+@if errorlevel 1 echo ==^> WARNING: Error %ERRORLEVEL% was returned by: netsh advfirewall firewall add rule name="Open Port 3389" dir=in action=allow protocol=TCP localport=3389
+
+:: Allow remote connections to this computer
+echo ==^> Allow remote connections to this computer
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+@if errorlevel 1 echo ==^> WARNING: Error %ERRORLEVEL% was returned by: reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
