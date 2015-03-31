d829d9c2b6925cabb6d1dfa051dd476e52e19f37
Ryan Hansohn <ryan.hansohn@gmail.com>
2015-03-31 16:18:09 -0700

    create c:\users\vagrant\.ssh path is missing

--- scripts/vagrant-ssh.bat
+++ scripts/vagrant-ssh.bat
@@ -1,4 +1,5 @@
 :: vagrant public key
+if not exist C:\Users\vagrant\.ssh mkdir C:\Users\vagrant\.ssh
 if exist a:\vagrant.pub (
   copy a:\vagrant.pub C:\Users\vagrant\.ssh\authorized_keys
 ) else (
