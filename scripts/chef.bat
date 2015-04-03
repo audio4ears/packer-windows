fd8d6493ac2f59965fb445a535319f4cbed7c048
Ryan Hansohn <ryan.hansohn@gmail.com>
2015-04-03 15:04:52 -0700

    modify errorlevel handling

--- scripts/chef.bat
+++ scripts/chef.bat
@@ -7,17 +7,17 @@ title Installing Chef Client. Please wait...
 :: Download chef client
 if not exist "C:\Windows\Temp\chef.msi" (
   echo ==^> Downloading Chef Client
-  powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://www.chef.io/chef/install.msi', 'C:\Windows\Temp\chef.msi')" > NUL
+  powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://www.chef.io/chef/install.msi', 'C:\Windows\Temp\chef.msi')" >nul
   @if errorlevel 1 echo ==^> WARNING: Error %ERRORLEVEL% was returned by: powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://www.chef.io/chef/install.msi', 'C:\Windows\Temp\chef.msi')"
 )
 
 :: Install chef client
-REG QUERY HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData\S-1-5-18\Products\1945256682812F8459C51AA05AD1CF7D > NUL
-if not %errorlevel% equ 0 (
+reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData\S-1-5-18\Products\1945256682812F8459C51AA05AD1CF7D" >nul
+if errorlevel 1 (
   echo ==^> Installing Chef Client
   msiexec /qb /i C:\Windows\Temp\chef.msi
   @if errorlevel 1 echo ==^> WARNING: Error %ERRORLEVEL% was returned by: msiexec /qb /i C:\Windows\Temp\chef.msi
 )
 
 :: Sleep
-powershell -Command "Start-Sleep 1" <NUL
+powershell -Command "Start-Sleep 1" >nul
