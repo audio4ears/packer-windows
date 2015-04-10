eee86c269468f712886d29c55d9df219778f3d5e
Ryan Hansohn <ryan.hansohn@gmail.com>
2015-04-10 11:59:22 -0700

    update reg query with output to Nul

--- scripts/chef.bat
+++ scripts/chef.bat
@@ -7,12 +7,12 @@ title Installing Chef Client. Please wait...
 :: Download chef client
 if not exist "C:\Windows\Temp\chef.msi" (
   echo ==^> Downloading Chef Client
-  powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://www.chef.io/chef/install.msi', 'C:\Windows\Temp\chef.msi')" >nul
+  powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://www.chef.io/chef/install.msi', 'C:\Windows\Temp\chef.msi')" > nul
   @if errorlevel 1 echo ==^> WARNING: Error %ERRORLEVEL% was returned by: powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://www.chef.io/chef/install.msi', 'C:\Windows\Temp\chef.msi')"
 )
 
 :: Install chef client
-reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData\S-1-5-18\Products\1945256682812F8459C51AA05AD1CF7D" >nul
+reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData\S-1-5-18\Products\1945256682812F8459C51AA05AD1CF7D" > nul 2>&1
 if errorlevel 1 (
   echo ==^> Installing Chef Client
   msiexec /qb /i C:\Windows\Temp\chef.msi
