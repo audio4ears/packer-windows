48b7967bff831154d2c15a5998fa3d7fbd498808
Ryan Hansohn <ryan.hansohn@gmail.com>
2015-10-18 23:37:22 -0700

    version bump

---
+++ scripts/microsoft-update.ps1
@@ -0,0 +1,29 @@
+#------------------------------------------------------------------------------
+# MAIN
+#------------------------------------------------------------------------------
+
+Write-Host "==> Enabling Microsoft Update";
+
+# stop windows update service
+Stop-Service -Name wuauserv -Force;
+
+# enable microsoft update
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update' `
+                 -Name EnableFeaturedSoftware `
+                 -PropertyType DWord `
+                 -Value 1 `
+                 -Force | Out-Null;
+
+# disable recomended updates (aka optional updates)
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update' `
+                 -Name IncludeRecommendedUpdates `
+                 -PropertyType DWord `
+                 -Value 0 `
+                 -Force | Out-Null;
+
+# register microsoft update with automatic updates
+$ServiceManager = New-Object -ComObject Microsoft.Update.ServiceManager -Strict;
+$ServiceManager.AddService2("7971f918-a847-4430-9279-4a52d1efe18d",7,"") | Out-Null;
+
+# start windows update service
+Start-Service -Name wuauserv;
